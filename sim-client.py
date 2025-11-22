4154f8629714c766529c62ae3c6027f7ab37f178
davichunlee <davichunlee@gmail.com>
2025-11-21 22:35:51 -0300

    algumas adicoes no server e client

--- sim-client.py
+++ sim-client.py
@@ -1,10 +1,24 @@
 import socket
+from bitarray import bitarray # type: ignore
 
-from requests import post
 
+# Cria socket client
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# associa a um IP e porta
 tcp_client.connect(('localhost', 9999))
 
-status = tcp_client.send(b'Hello, Server!')
+# manda mensagem
+data = input("Fale com o mundo: ")
+by_data = data.encode('utf-8')
+bits = bitarray()
+bits.frombytes(by_data)
+print('Bits: ',  bits)
+print('Cru: ',  by_data)
+status = tcp_client.send(by_data)
 
+# printa status
 print(f'Sent {status} bytes to the server.')
+
+# fecha o socket
+tcp_client.close()
